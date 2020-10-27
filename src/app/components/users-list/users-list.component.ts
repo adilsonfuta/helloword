35f3530031508479b9787f4ef96a1d77c1658412
adilson.futa <adilson.futa@jupiter.co.ao>
2020-10-27 19:17:54 +0000

    fazendo uso de template drive form

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/model/User';
 
 
@@ -9,156 +9,135 @@ import { User } from 'src/app/model/User';
 })
 export class UsersListComponent implements OnInit {
 
-   // isActive:true,
-    // registered: new Date('01/02/2020 08:30:00'),
-    // hide:true
+  // isActive:true,
+  // registered: new Date('01/02/2020 08:30:00'),
+  // hide:true
 
-  user:User={
+  user: User = {
     firstname: '',
     lastname: '',
-    age: null,
-    address: {
-        street: '',
-        city: '',
-        state: ''
-    }
+    email: ''
   };
+
   users: User[];
-  showExtended: boolean=true;  
-  loaded: boolean= false; 
-  enableAdd:boolean=false;
-  showUserForm:boolean=false;
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enableAdd: boolean = false;
+  showUserForm: boolean = false;
+  @ViewChild('userForm') form: any;
 
 
-//  o construtor usar 
-//  para  injecao depencia
+  //  o construtor usar 
+  //  para  injecao depencia
   constructor() {
-    
-   }
+
+  }
 
   // permite ver o lifecicle do 
   //components podes usar ele para req ajax
- 
-  ngOnInit() { 
-    this.users=[
+
+  ngOnInit() {
+    this.users = [
       {
         firstname: 'Adilson',
-    lastname: 'Futa',
-    age: 78,
-    address: {
-        street: ' Km 14 A',
-        city: 'Viana',
-        state: 'Distrito de Urbano de Luanda'
-    }, 
-    isActive:true,
-    registered: new Date('01/02/2020 08:30:00'),
-    hide:true
-    },
+        lastname: 'Futa',
+        email: 'adilson@gmail.com',
+        isActive: true,
+        registered: new Date('01/02/2020 08:30:00'),
+        hide: true
+      },
 
-    {
-      firstname: 'Irene',
-  lastname: 'Futa',
-  age: 25,
-  address: {
-      street: ' Km 14 A',
-      city: 'Viana',
-      state: 'Distrito de Urbano de Luanda'
-  }, 
-  isActive:false,
-  registered: new Date('03/10/2020 18:20:00'),
-  hide:true
-  },
+      {
+        firstname: 'Irene',
+        lastname: 'Futa',
+        email: 'irene@gmail.com',
+        isActive: false,
+        registered: new Date('03/10/2020 18:20:00'),
+        hide: true
+      },
 
-  {
-    firstname: 'Luzilda',
-lastname: 'Futa',
-age: 19,
-address: {
-    street: ' Km 14 A',
-    city: 'Viana',
-    state: 'Distrito de Urbano de Luanda'
-}, 
-isActive:true,
-registered: new Date('11/08/2020 10:30:00'),
-hide:true
+      {
+        firstname: 'Luzilda',
+        lastname: 'Futa',
+        email: 'luzilda@gmail.com',
+        isActive: true,
+        registered: new Date('11/08/2020 10:30:00'),
+        hide: true
+      }
+    ];
+
+
+    this.loaded = true;
+    //this.showExtended=false;
+
+  }
+
+  // FUNCOES EXTRAS 
+
+
+  // addUser() {
+  //   this.user.isActive = true;
+  //   this.user.registered = new Date();
+  //   this.users.unshift(this.user);
+
+  //   this.user = {
+  //     firstname: '',
+  //     lastname: '',
+  //     email: ''
+  //   };
+  // }
+
+  toggleHide(u: User) {
+    u.hide = !u.hide;
+  }
+
+  onSubmit({ value, valid }: { value: User, valid: boolean }) {
+    if (!valid) {
+      console.log('form invalido');
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
+      this.users.unshift(value);
+      this.form.reset();
+    }
+  }
+
+
+
 }
-]; 
 
-
-this.loaded=true;
-//this.showExtended=false;
 
 /*
-
-this.addUser( 
+  fireEvent(e) {
+    console.log(e.type);
+    console.log(e.target.value);
+  }
+this.addUser(
 {
     firstname: 'Afonso',
     lastname: 'Futa'
 });
+
+addUser( user: User){
+    this.users.push(user);
+  }
 */
 
+/*    setTimeout(()=>{
+    codigo carregadni fica aqui
 
-}
-
-
-  // addUser( user: User){
-  //   this.users.push(user);
-  // }
-
-
-  addUser( ){
-    this.user.isActive=true;
-    this.user.registered=new Date();
-        this.users.unshift(this.user);
-
-    this.user={
-      firstname: '',
-      lastname: '',
-      age: null,
-      address: {
-          street: '',
-          city: '',
-          state: ''
-      }
-    };
-  }
-
-  toggleHide(u: User){    
-    u.hide=!u.hide;
-  }
-
-  onSubmit(e){
-
-    console.log("1212")
-    e.preventDefault();
-
-  }
-
-  fireEvent(e){
-
-    console.log(e.type);
-    console.log(e.target.value);
- 
-
-  }
- 
-}
-
-
-/*    setTimeout(()=>{  
-    codigo carregadni fica aqui   
-   
 },2000);
 */
 
 
- /*this.addUser( {
-      firstname: 'Afonso',
-  lastname: 'Futa',
-  age: 13,
-  address: {
-      street: ' Km 14 A',
-      city: 'Viana',
-      state: 'Distrito de Urbano de Luanda'
-  }
-  })*/
+/*this.addUser( {
+     firstname: 'Afonso',
+ lastname: 'Futa',
+ age: 13,
+ address: {
+     street: ' Km 14 A',
+     city: 'Viana',
+     state: 'Distrito de Urbano de Luanda'
+ }
+ })*/
